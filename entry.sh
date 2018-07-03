60cbc84f29867944acb999696006979f842b8dba
Jason Rivers <jason@jasonrivers.co.uk>
2018-07-03 11:18:52 +0100

    Update MiServer location

--- entry.sh
+++ entry.sh
@@ -7,6 +7,6 @@ else
     git pull
 fi
 
-export MiServer=${MiServer-/MiServer/SampleMiSites/MS3}
-dyalog -ride +s /MiServer/miserver.dws 0</tmp/aplfifo 
+export MiServer=${MiServer-/MiServer/MS3}
+dyalog -ride +s /MiServer/miserver.dws 0<&-
 
